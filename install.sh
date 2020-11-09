dd641db2b7e88333853795630552e02ccbb122a7
Anwesha Tomar <atomar19@gwu.edu>
2020-11-09 15:37:43 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+pip3 install --upgrade google-cloud-speech
+
+pip3 install deepspeech
+pip3 install deepspeech-gpu
+
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer
+
+pip3 install jiwer
