ca266489d6725c80cfa7d75bf8c5b862dd2ee7f7
Anwesha Tomar <atomar19@gwu.edu>
2020-11-20 12:55:33 -0500

    Update install.sh

--- speechrecog/install.sh
+++ speechrecog/install.sh
@@ -4,8 +4,8 @@ sudo -H pip3 install --upgrade google-cloud-speech
 sudo -H pip3 install deepspeech
 sudo -H pip3 install deepspeech-gpu
 
-wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm
-wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.1/deepspeech-0.9.1-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.1/deepspeech-0.9.1-models.scorer
 #WER
 sudo -H pip3 install jiwer
 #sox
